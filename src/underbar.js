86c548b055d18fcf5218ae3827cac5067f8a6b76
John Anderson <jfanderson373@gmail.com>
2015-07-07 21:53:17 -0500

    "filter" and "reject" pass all tests

--- src/underbar.js
+++ src/underbar.js
@@ -77,12 +77,26 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var passed = [];
+    _.each(collection, function(item) {
+      if (test(item)) {
+        passed.push(item);
+      }
+    });
+    return passed;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(item) {
+      if (test(item)) {
+        return false;
+      } else {
+        return true;
+      }
+    })
   };
 
   // Produce a duplicate-free version of the array.
